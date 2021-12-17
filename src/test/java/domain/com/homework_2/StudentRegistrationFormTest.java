478e4989fcc9fcaa91dea1da3cc4ef28d78708fe
Vitalii Ismagilov <ismagilov.vel@gmail.com>
2021-12-17 21:31:02 +0200

    Final version with different options of locators

--- src/test/java/domain/com/homework_2/StudentRegistrationFormTest.java
+++ src/test/java/domain/com/homework_2/StudentRegistrationFormTest.java
@@ -5,7 +5,7 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.CollectionCondition.exactTexts;
-import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
 
@@ -13,6 +13,7 @@ public class StudentRegistrationFormTest {
 
     @BeforeAll
     static void beforeAll() {
+        Configuration.baseUrl = "https://demoqa.com";
         Configuration.holdBrowserOpen = false;
         Configuration.headless = false;
     }
@@ -20,22 +21,26 @@ public class StudentRegistrationFormTest {
     @Test
     void formShouldBeSubmittedAndVerified() {
         // Arrange
-        open("https://demoqa.com/automation-practice-form");
+        open("/automation-practice-form");
 
         // Act
         $("#firstName").setValue("Vitaliy");
         $("#lastName").setValue("Ismagilov");
         $("#userEmail").setValue("ism@gmail.com");
-        $(byText("Male")).click();
+
+        $("#genterWrapper").$(byText("Male")).click();
+        //$("#gender-radio-1").parent().click(); // second option
+
         $("#userNumber").setValue("3777448888");
 
         $("#dateOfBirthInput").click();
-        $(".react-datepicker__month-select").click();
         $(".react-datepicker__month-select").selectOption("April");
         $(".react-datepicker__year-select").selectOption("1984");
-        $(byText("29")).click();
-
-        //executeJavaScript(String.format("$('[id=\"%s\"]').val('%s')", "dateOfBirthInput", "29 Apr 1984"));
+        //$(byText("29")).click(); // option 1
+        //$(".react-datepicker__day--029:not(.react-datepicker__day--outside-month").click(); // option 2
+        //$("[aria-label$='July 30th, 2008']").click(); // option 3
+        $$(".react-datepicker__day--029").filter(not(cssClass(".react-datepicker__day--outside-month"))).first().click(); // option 4
+        //executeJavaScript(String.format("$('[id=\"%s\"]').val('%s')", "dateOfBirthInput", "29 Apr 1984")); // option 5
 
         $("#subjectsInput").hover().setValue("Ma").pressEnter();
         $("#subjectsInput").setValue("En").pressEnter();
