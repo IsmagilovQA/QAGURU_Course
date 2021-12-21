6e16b6d877d630fa15194f2416536aff96b5249a
Vitalii Ismagilov <ismagilov.vel@gmail.com>
2021-12-21 10:33:15 +0200

    Cleaned up final version of HW 1

--- src/test/java/domain/com/homework_2/StudentRegistrationFormTest.java
+++ src/test/java/domain/com/homework_2/StudentRegistrationFormTest.java
@@ -2,6 +2,7 @@ package domain.com.homework_2;
 
 import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.CollectionCondition.exactTexts;
@@ -18,29 +19,27 @@ public class StudentRegistrationFormTest {
         Configuration.headless = false;
     }
 
+    @BeforeEach
+    void setup() {
+        open("/automation-practice-form");
+    }
+
     @Test
     void formShouldBeSubmittedAndVerified() {
-        // Arrange
-        open("/automation-practice-form");
-
         // Act
         $("#firstName").setValue("Vitaliy");
         $("#lastName").setValue("Ismagilov");
         $("#userEmail").setValue("ism@gmail.com");
 
         $("#genterWrapper").$(byText("Male")).click();
-        //$("#gender-radio-1").parent().click(); // second option
 
         $("#userNumber").setValue("3777448888");
 
         $("#dateOfBirthInput").click();
         $(".react-datepicker__month-select").selectOption("April");
         $(".react-datepicker__year-select").selectOption("1984");
-        //$(byText("29")).click(); // option 1
-        //$(".react-datepicker__day--029:not(.react-datepicker__day--outside-month").click(); // option 2
-        //$("[aria-label$='July 30th, 2008']").click(); // option 3
-        $$(".react-datepicker__day--029").filter(not(cssClass(".react-datepicker__day--outside-month"))).first().click(); // option 4
-        //executeJavaScript(String.format("$('[id=\"%s\"]').val('%s')", "dateOfBirthInput", "29 Apr 1984")); // option 5
+        $$(".react-datepicker__day--029").filter(not(cssClass(".react-datepicker__day--outside-month")))
+                .first().click();
 
         $("#subjectsInput").hover().setValue("Ma").pressEnter();
         $("#subjectsInput").setValue("En").pressEnter();
@@ -63,11 +62,6 @@ public class StudentRegistrationFormTest {
         $$(".table td:last-child").shouldHave(exactTexts("Vitaliy Ismagilov", "ism@gmail.com",
                 "Male", "3777448888", "29 April,1984", "Maths, English", "Music, Sports", "simple.txt",
                         "London is a capital of Great Britain", "Haryana Panipat"));
-
-        // Second version of assert
-//        $(".table").shouldHave(text("Vitaliy Ismagilov"), text("ism@gmail.com"), text("Male"),
-//                text("3777448888"), text("29 April,1984"), text("Maths, English"), text("Music, Sports"),
-//                text("simple.txt"), text("London is a capital of Great Britain"), text("Haryana Panipat"));
 
     }
 }
