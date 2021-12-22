224367fd9306675624f86990bd3a54a78f685b69
Vitalii Ismagilov <ismagilov.vel@gmail.com>
2021-12-22 12:38:47 +0200

    Tasks for homework 3

---
+++ src/test/java/domain/com/homework_3/Homework3Tests.java
@@ -0,0 +1,74 @@
+package domain.com.homework_3;
+
+import com.codeborne.selenide.ElementsCollection;
+import com.codeborne.selenide.Selenide;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selenide.*;
+
+public class Homework3Tests {
+
+    /**
+     * 1. Есть ли разница между $("h1 div"); и $("h1").$("div"); - может ли привести к тому что, поиск найдёт разные элементы?
+     * Если может - приведите пример, когда.
+     *
+     * Ответ:
+     *  Да, может быть ситуация, когда эти два локатора найдут разные элмементы (div блоки). Как пример:
+     *  $("h1 div") -> этот локатор ищет все вложенные div элементы внутри h1 не только первого уровня вложенности,
+     *  но и всех последующих вложенностей, а это подход $("h1").$("div") означает, что div будет искаться исключительно
+     *  среди вложенных элементов внутри h1 только непосредственно на первом уровне вложенности.
+     *  По сути $("h1").$("div") это аналог CSS селектора $("h1 > div").
+     */
+
+
+    /**
+     *2. Разработайте следующий автотест:
+     *  - Откройте страницу Selenide в Github
+     *  - Перейдите в раздел Wiki проекта
+     *  - Убедитесь, что в списке страниц (Pages) есть страница SoftAssertions
+     *  - Откройте страницу SoftAssertions, проверьте что внутри есть пример кода для JUnit5
+     */
+
+    @Test
+    void shouldBeJunitSnippet() {
+        open("https://github.com/selenide/selenide");
+        $("#wiki-tab").click();
+        $("#wiki-pages-filter").setValue("SoftAssertions");
+        $(By.linkText("SoftAssertions")).shouldBe(visible).click();
+        $$(".markdown-body > ol").findBy(text("Using JUnit5 extend test class:")).sibling(0)
+                .shouldBe(visible);
+    }
+
+
+    /**
+     * 3. (опциональное) Запрограммируйте Drag&Drop с помощью Selenide.actions()
+     *  - Откройте https://the-internet.herokuapp.com/drag_and_drop
+     *  - Перенесите прямоугольник А на место В
+     *  - Проверьте, что прямоугольники действительно поменялись
+     */
+
+    @Test
+    void dragAndDropWithSelenide() {
+        open("https://the-internet.herokuapp.com/drag_and_drop");
+        $$("#columns > div").first().shouldHave(text("A"));
+        $("#column-a").dragAndDropTo("#column-b");
+        $$("#columns > div").first().shouldHave(text("B"));
+    }
+
+
+    // This option doesn't work. Block A is moved, but not released to target block B.
+    @Test
+    void dragAndDropSecondOption() {
+        open("https://the-internet.herokuapp.com/drag_and_drop");
+        $$("#columns > div").first().shouldHave(text("A"));
+        WebElement a = $("#column-a");
+        WebElement b = $("#column-b");
+        Selenide.actions().dragAndDrop(a, b).perform();
+        $$("#columns > div").first().shouldHave(text("B"));
+
+    }
+}
