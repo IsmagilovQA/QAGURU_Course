87418deb86f4964a17090e70d9599b76ff2c5da3
Vitalii Ismagilov <ismagilov.vel@gmail.com>
2022-01-31 09:30:31 +0200

    This is a squashed commit

---
+++ src/test/java/domain/com/homework_9/TestClassForGit.java
@@ -0,0 +1,18 @@
+package domain.com.homework_9;
+
+import org.junit.jupiter.api.Test;
+
+import java.sql.SQLOutput;
+
+public class TestClassForGit {
+
+    /**
+     * Here can be some comments.
+     */
+
+    @Test
+    void testMethod() {
+        System.out.println("simple test method");
+        String str = "It's a string, you can use it!";
+    }
+}
